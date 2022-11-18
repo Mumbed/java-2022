570fceb0e417b2038005afbb82ea0a111fb72f32
hs-1971432-youngsinwon <dudtlsdl00@hansung.ac.kr>
2022-11-18 22:49:20 +0900

    drawing, font etc.. commit

---
+++ Draw/src/paintCanvas.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
+public class paintCanvas extends JFrame {
+    static final int width = 50;
+    static final int height = 50;
+    int x ,y;
+    ArrayList<Point> pointlist = new ArrayList<Point>();
+    int dragID = -1;
+
+    public paintCanvas(){
+        super("Example");
+        setLocation(500,200);
+        setSize(400,300);
+        setTitle("Paine EX");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+//    private void registerEventListener(){
+//        addMouseListener(new MouseAdapter(){
+//            public void mousePressed(MouseEvent e) {
+//                int x = e.getX();
+//                int y = e.getY();
+//                repaint();
+//            }
+//        });
+//        addMouseMotionListener(new MouseMotionAdapter(){
+//
+//        });
+//    }
+    public void paint(Graphics g){
+        Color c = new Color(255,0,0);
+        g.setColor(c);
+
+        addMouseListener(new MouseAdapter(){
+            public void mousePressed(MouseEvent e) {
+                x = e.getX();
+                y = e.getY();
+                repaint();
+            }
+        });
+        g.fillOval(x,y,width,height);
+    }
+    public static void main(String[] args){
+        new paintCanvas();
+    }
+}
