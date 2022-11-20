b03b9e2296c559ff210a4ccac624cfa3dfb6d906
hs-1971432-youngsinwon <dudtlsdl00@hansung.ac.kr>
2022-11-20 13:00:42 +0900

    JMenu

--- Draw/src/paintCanvas.java
+++ Draw/src/paintCanvas.java
@@ -4,7 +4,8 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.util.ArrayList;
-public class paintCanvas extends JFrame {
+public class paintCanvas extends JComponent {
+    JFrame frame = new JFrame();
     static final int width = 50;
     static final int height = 50;
     int x ,y;
@@ -12,12 +13,13 @@ public class paintCanvas extends JFrame {
     int dragID = -1;
 
     public paintCanvas(){
-        super("Example");
-        setLocation(500,200);
-        setSize(400,300);
-        setTitle("Paine EX");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+
+        this.frame.setLocation(500,200);
+
+        this.frame.setSize(400,300);
+        this.frame.setTitle("Paint EX");
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setVisible(true);
     }
 //    private void registerEventListener(){
 //        addMouseListener(new MouseAdapter(){
@@ -32,9 +34,9 @@ public class paintCanvas extends JFrame {
 //        });
 //    }
     public void paint(Graphics g){
+        super.paint(g);
         Color c = new Color(255,0,0);
         g.setColor(c);
-
         addMouseListener(new MouseAdapter(){
             public void mousePressed(MouseEvent e) {
                 x = e.getX();
